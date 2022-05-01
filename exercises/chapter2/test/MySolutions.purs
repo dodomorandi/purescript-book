0f44aeb012bd762de736f6b078870fcd39773ea4
Edoardo Morandi <morandidodo@gmail.com>
2022-05-01 23:38:55 +0200

    Solve chapter 2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,11 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.Number (sqrt, pi)
+import Data.Int (rem)
+
+diagonal w h = sqrt $ w * w + h * h
+
+circleArea r = r * r * pi
+
+leftoverCents n = n `rem` 100
